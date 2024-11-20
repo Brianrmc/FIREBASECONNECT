3d9014713ef7f80dcd86213adae0053844445943
Brian Manriquez <al23320276@utcj.edu.mx>
2024-11-20 12:27:04 -0700

    se modificaron componentes de js para conectar a firebase

--- firebaseconect.js
+++ firebaseconect.js
@@ -1,5 +1,6 @@
 <script type="module" src="./firebaseconect.js"></script>
 
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/13.25.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/13.25.0/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
